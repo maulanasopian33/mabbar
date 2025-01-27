3fed05014630b0457c2de251fdbe1c6f340dd6a6
Maulana Sopian <maulana@antmediahost.com>
2025-01-27 09:56:53 +0700

    :tada: init

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: 'light',
+            cssLayer: {
+                name: 'primevue',
+                order: 'tailwind-base, primevue, tailwind-utilities'
+            }
+        }
+    }
+});
+app.mount('#app')
